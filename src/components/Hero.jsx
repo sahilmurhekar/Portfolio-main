4ddac70d6d6bc921baadb2f949c48cc094164fbb
Upayan M <upayanm3@gmail.com>
2025-06-12 17:20:57 +0530

    feat: add .hintrc configuration and include react-icons dependency

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { useInView } from "react-intersection-observer";
-import BlurText from "./BlurText";
 import GradientText from "./GradientText";
+import { FaCircleChevronDown } from "react-icons/fa6";
+
 const Hero = () => {
-  // Define refs and inView states for each animated section
   const { ref: titleRef, inView: titleInView } = useInView({
     triggerOnce: true,
     threshold: 0.3,
@@ -30,24 +29,21 @@ const Hero = () => {
   return (
     <div
       id="top"
-      className="flex flex-col items-center justify-center overflow-hidden pt-36"
+      className="h-screen flex flex-col items-center justify-center overflow-hidden px-4"
     >
       <div
         ref={titleRef}
-        className={`pt-42 ${
-          titleInView
-            ? "animate__animated animate__fadeInUp animate__delay-0s"
-            : "opacity-0"
+        className={`${
+          titleInView ? "animate__animated animate__fadeInUp" : "opacity-0"
         }`}
       >
         <h4 className="text-center text-sm md:text-md lg:text-lg">Hello I'm</h4>
       </div>
+
       <div
         ref={nameRef}
         className={`pt-4 ${
-          nameInView
-            ? "animate__animated animate__zoomIn animate__delay-0s"
-            : "opacity-0"
+          nameInView ? "animate__animated animate__zoomIn" : "opacity-0"
         }`}
       >
         <GradientText
@@ -58,12 +54,11 @@ const Hero = () => {
           <h1>SAHIL MURHEKAR</h1>
         </GradientText>
       </div>
+
       <div
         ref={descRef}
-        className={`pt-4 w-82 md:w-120 ${
-          descInView
-            ? "animate__animated animate__fadeInUp animate__delay-0s"
-            : "opacity-0"
+        className={`pt-4 max-w-3xl ${
+          descInView ? "animate__animated animate__fadeInUp" : "opacity-0"
         }`}
       >
         <h4 className="text-center text-sm md:text-md lg:text-lg">
@@ -73,20 +68,17 @@ const Hero = () => {
           connect technology, creativity, and strategy.
         </h4>
       </div>
+
       <div
         ref={iconRef}
-        className={`py-32 ${
-          iconInView
-            ? "animate__animated animate__bounce animate__delay-0s"
-            : "opacity-0"
+        className={`pt-8 ${
+          iconInView ? "animate__animated animate__bounce" : "opacity-0"
         }`}
       >
         <a href="#about">
-          <img
-            style={{ transition: "300ms" }}
+          <FaCircleChevronDown
             className="floating-icon cursor-pointer hover:opacity-60"
-            src="/assets/down.png"
-            alt=""
+            size={40}
           />
         </a>
       </div>
